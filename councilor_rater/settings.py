0ef2d1f7a5a71286668413b77f106f47d8a70547
Sicelumusa <musaqwabe@gmail.com>
2024-02-20 17:11:07 +0200

    Linked the front end

--- councilor_rater/settings.py
+++ councilor_rater/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", "https://crep-63b35.web.app", "http://crep-63b35.web.app"
+    "http://localhost:3000", "https://crep-7896a.web.app", "http://crep-7896a.web.app"
 ]
 
 ROOT_URLCONF = 'councilor_rater.urls'

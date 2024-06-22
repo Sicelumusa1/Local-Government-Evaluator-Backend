2a3a953c1cc8f9b4786c0e03d5374afe5e61379b
Sicelumusa <musaqwabe@gmail.com>
2024-06-22 13:34:38 +0200

    Enhances security when sending an email

--- councilor_rater/settings.py
+++ councilor_rater/settings.py
@@ -166,3 +166,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True

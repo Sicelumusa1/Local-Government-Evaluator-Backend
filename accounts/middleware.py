b315f118f4bdcd0390920e0ea46239dbd5568983
Sicelumusa <musaqwabe@gmail.com>
2024-07-02 14:23:54 +0200

    Added error handling

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -11,5 +11,14 @@ class JWTAuthenticationMiddleware(MiddlewareMixin):
                 access_token = AccessToken(token)
                 user_id = access_token['user_id']
                 request.user = Account.objects.get(id=user_id)
-            except Exception as e:
+            except TokenError as e:
+                print(f'Token decoding error: {e}')
                 request.user = AnonymousUser()
+            except Account.DoesNotExist:
+                print(f'User with id {user_id} does not exist')
+                request.user = AnonymousUser()
+            except Exception as e:
+                print(f'Unexpected error: {e}')
+                request.user = AnonymousUser()
+        else:
+            request.user = AnonymousUser()

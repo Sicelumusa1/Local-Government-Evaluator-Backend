b8e673df745bf6edba6ad6b24eab06d81c755d95
Sicelumusa <musaqwabe@gmail.com>
2024-07-17 19:27:45 +0200

    Altered the middleware to be compatible with the django version

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,12 +1,26 @@
-from django.utils.deprecation import MiddlewareMixin
-# from rest_framework_simplejwt.authentication import JWTAuthentication
-# from rest_framework_simplejwt.exceptions import InvalidToken
-# from django.contrib.auth.models import AnonymousUser
-# from .models import Account
+from rest_framework_simplejwt.tokens import AccessToken, TokenError
+from django.contrib.auth.models import AnonymousUser
+from .models import Account
 
-class JWTAuthenticationMiddleware(MiddlewareMixin):
-    def process_request(self, request):
+class JWTAuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+
+    def __call__(self, request):
         access_token = request.COOKIES.get('access_token')
         if access_token:
-            request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
-            
+            try:
+                token = AccessToken(access_token)
+                user_id = token['user_id']
+                request.user = Account.objects.get(id=user_id)
+            except TokenError as e:
+                print(f"Token decoding error: {e}")
+                request.user = AnonymousUser()
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                request.user = AnonymousUser()
+        else:
+            request.user = AnonymousUser()
+        response = self.get_response(request)
+        return response
